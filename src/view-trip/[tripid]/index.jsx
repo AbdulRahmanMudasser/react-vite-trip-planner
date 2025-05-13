cdbf7fa558dd415ad87a6f6bfbf64d8284ff749a
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-05-14 03:50:36 +0500

    travel guide chatbot added in view trip page

--- src/view-trip/[tripid]/index.jsx
+++ src/view-trip/[tripid]/index.jsx
@@ -2,44 +2,45 @@ import { db } from '@/service/firebaseConfig'
 import { doc, getDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import { toast } from 'react-hot-toast'; // Added import for toast
 import InfoSection from './components/InfoSection';
 import Hotels from './components/Hotels';
 import PlacesToVisit from './components/PlacesToVisit';
 import Footer from './components/Footer';
+import TravelExpertChatbot from './components/TravelExpertChatbot';
 
 function Viewtrip() {
-
     const {tripId} = useParams();
     const [trip, setTrip] = useState([])
-    useEffect(()=>{
-        tripId&&GetTripData();
-    },[tripId])
+    
+    useEffect(() => {
+        tripId && GetTripData();
+    }, [tripId])
 
     // Used to get trip information from firebase 
-
     const GetTripData = async() => {
-        const docRef = doc(db, 'AITrips', tripId );
+        const docRef = doc(db, 'AITrips', tripId);
         const docSnap = await getDoc(docRef);
 
         if(docSnap.exists()){
-            console.log("Document:",docSnap.data());
+            console.log("Document:", docSnap.data());
             setTrip(docSnap.data())
         }
         else{
             console.log("No Such Document");
-            toast('No trip FOund!')
+            toast('No trip found!')
         }
     }
   
     return (
-    <div>
-        <InfoSection trip={trip} />
-        <Hotels trip={trip} />
-        <PlacesToVisit trip={trip} />
-        <Footer trip={trip}/>
-    </div>
-    
-  )
+        <div>
+            <InfoSection trip={trip} />
+            <Hotels trip={trip} />
+            <PlacesToVisit trip={trip} />
+            <Footer trip={trip}/>
+            <TravelExpertChatbot tripData={trip} />
+        </div>
+    )
 }
 
 export default Viewtrip
